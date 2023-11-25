7dc04352ad1ba6b93b065a5c1cdcfcde569af6e9
YuChen <18220176924@163.com>
2023-11-25 12:42:24 +0800

    添加语音识别

---
+++ src/components/Agent/index.js
@@ -0,0 +1,41 @@
+import {useState} from "react";
+import { ANSWERS } from '../../constants/index'
+import agentImg from '../../assets/agent.png'
+import './index.css'
+
+export const Agent = () => {
+  const [msg, setMsg] = useState('')
+  const [showModal, setShowModal] = useState(false)
+
+  //语音识别
+  const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+  const newRecognition = new SpeechRecognition()
+  newRecognition.lang = 'zh-CN';
+  newRecognition.continuous = true;
+  newRecognition.interimResults = false;//true;
+
+  newRecognition.onresult = ({ results }) => {
+    const { transcript, confidence } = results[results.length - 1][0];
+    setMsg(transcript)
+    setShowModal(true)
+    if(ANSWERS[transcript]) {
+      // 播放录音
+    }
+  }
+  newRecognition.onend = () => { newRecognition.start() }
+
+  const handleClick = () => {
+    newRecognition.start()
+  }
+
+
+  return (<div>
+    <button onClick={handleClick}>开始</button>
+    <div className={['agent-modal', showModal? 'fade-in': ''].join(' ')}>
+      {msg}
+      <div className="agent-icon">
+        <img src={agentImg}/>
+      </div>
+    </div>
+  </div>)
+}
---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const ANSWERS = {
+  '你好': 'Eric,早上好！今天周一，要导航去公司吗',
+  '是的': '好的，为您导航到公司，预计二十分钟后到达',
+
+}
+
+export const AUTO_DETECTION = {
+  fatigueDriving: '检测到您已驾驶超过4小时，前方12km处是临潼服务区，建议主人及时休息，避免疲劳驾驶',
+  drinkDriving: '检测到车身偏移摇晃，行车轨迹不正，主人您可能醉酒，建议立即靠路边安全停车'
+}
