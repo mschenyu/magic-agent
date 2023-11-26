eddee3606f781ba7304b5f0f4dd18f47c832811e
alimjanwicresoft <alimjana@wicresoft.com>
2023-11-26 09:51:33 +0800

    Changes background.

--- src/components/DialogModal/index.js
+++ src/components/DialogModal/index.js
@@ -1,14 +1,43 @@
 import agentImg from "../../assets/agent.png";
+import agentImgInquire01 from "../../assets/agent-inquire-01.png";
+import agentImgInquire02 from "../../assets/agent-inquire-02.png";
+import agentImgwarning from "../../assets/agent-safety-warning.png";
 import './index.css'
 
 
-export const DialogModal = ({showModal, msg}) => {
+export const DialogModal = ({ showModal, msg, onIconClick, agentIconState }) => {
   return (<div>
-    <div className={['agent-modal', showModal? 'fade-in': ''].join(' ')}>
+    <div className={['agent-modal', showModal ? 'fade-in' : ''].join(' ')}>
       {msg}
-      <div className="agent-icon">
-        <img src={agentImg}/>
-      </div>
+      <AgentIcon state={agentIconState} onIconClick={onIconClick} />
     </div>
+  </div>)
+}
+
+export const AgentIcon = ({ state, onIconClick }) => {
+  let agentImgSrc = agentImg
+  switch (state) {
+    case 'inquire1':
+      agentImgSrc = agentImgInquire01
+      break;
+    case 'inquire2':
+      agentImgSrc = agentImgInquire02
+      break;
+    case 'warning':
+      agentImgSrc = agentImgwarning
+      break;  
+    default:
+      agentImgSrc = agentImg
+      break;
+  }
+  return (
+    <div className="agent-icon" >
+      <img src={agentImgSrc} onClick={onIconClick}/>
+    </div>)
+}
+
+export const Background = ({ children, isActive }) => {
+  return (<div className={isActive ? 'backgound-active' : 'backgound-inactive'}>
+    {children}
   </div>)
 }
--- src/pages/story2/index.js
+++ src/pages/story2/index.js
@@ -1,6 +1,98 @@
-import { Agent } from "../../components/Agent"
-import { STORY2_ANSWERS } from "../../constants/index"
+import { useEffect, useRef, useState } from "react";
+import agentImg from '../../assets/agent.png'
+import './index.css'
+import { Background, DialogModal } from "../../components/DialogModal";
+
+import usecase01Dialog01 from '../../assets/audio/storyline02/zh-CN-XiaoxiaoNeural-storyline02-usecase01-dialog-01.mp3'
+import usecase01Dialog02 from '../../assets/audio/storyline02/zh-CN-XiaoxiaoNeural-storyline02-usecase01-dialog-02.mp3'
+import usecase01Dialog03 from '../../assets/audio/storyline02/zh-CN-XiaoxiaoNeural-storyline02-usecase01-dialog-03.mp3'
+
+import usecase02Dialog01 from '../../assets/audio/storyline02/zh-CN-XiaoxiaoNeural-storyline02-usecase02-dialog-01.mp3'
+import usecase02Dialog02 from '../../assets/audio/storyline02/zh-CN-XiaoxiaoNeural-storyline02-usecase02-dialog-02.mp3'
+import usecase02Dialog03 from '../../assets/audio/storyline02/zh-CN-XiaoxiaoNeural-storyline02-usecase02-dialog-03.mp3'
 
 export const Story2 = () => {
-  return <Agent dictionary={STORY2_ANSWERS} />
+  const [msg, setMsg] = useState('')
+  const [showModal, setShowModal] = useState(false)
+  const [agentIconState, setAgentIconState] = useState('default')
+  const [isStart, setIsStart] = useState(false)
+  const [isActive, setisActive] = useState(false)
+  useEffect(() => {
+    setTimeout(()=>{
+    }, 1000)
+  })
+  const audioRef = useRef(null)
+
+  const start = () => {
+    setIsStart(true)
+    setisActive(true)
+    usecaseO1(setShowModal, setMsg, audioRef);
+  }
+
+  function usecaseO1(setShowModal, setMsg, audioRef) {
+    setShowModal(true);
+    setAgentIconState('inquire1')
+    setMsg("嗨Eric，你是不是喝酒了？检测到驾车轨迹不稳车辆摇晃异常，有酒驾风险，建议尽快停车。需要帮你联系最近的代驾吗？");
+    audioRef.current.src = usecase01Dialog01;
+    audioRef.current.play();
+    setTimeout(() => {
+      setMsg("额，你在说什么？");
+    }, 15000);
+  
+    setTimeout(() => {
+      setMsg("酒驾有危险，需要对自己和他人负责。建议在200米前方停车。第二次确认需要酒驾吗？");
+      audioRef.current.src = usecase01Dialog02;
+      audioRef.current.play();
+    }, 20000);
+  
+    setTimeout(() => {
+      setMsg("不需要，我没醉。");
+    }, 30000);
+  
+    setTimeout(() => {
+      setMsg("那我很抱歉了，要小心驾驶哦");
+      audioRef.current.src = usecase01Dialog03;
+      audioRef.current.play();
+      usecaseO2(setShowModal, setMsg, audioRef);
+    }, 38000);
+  }
+  
+  function usecaseO2(setShowModal, setMsg, audioRef) {
+    setTimeout(() => {
+      setMsg("啊！");
+    }, 8000);
+  
+    setTimeout(() => {
+      audioRef.current.src = usecase02Dialog01;
+      audioRef.current.play();
+      setMsg("Eric你还清醒吗？检测到发生了车祸碰撞，需要为你报警吗？");
+    }, 12000);
+  
+    setTimeout(() => {
+      audioRef.current.src = usecase02Dialog02;
+      audioRef.current.play();
+      setMsg("Eric你受伤了，系统已自动拨打120急救电话。");
+    }, 20000);
+  
+    setTimeout(() => {
+      audioRef.current.src = usecase02Dialog03;
+      audioRef.current.play();
+      setMsg("已为你拨打120，预计5分钟后救援到");
+    }, 28000);
+  
+    setTimeout(() => {
+      setMsg("");
+      setShowModal(false)
+    }, 35000);
+  }
+
+
+  return (<div className='story-page' >
+    <Background isActive={isActive} />
+    <DialogModal showModal={showModal} msg={msg} onIconClick={start} agentIconState={agentIconState}/>
+    <audio ref={audioRef} >
+      <source type="audio/mpeg" />
+    </audio>
+  </div>)
 }
+
